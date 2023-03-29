da09a71a0c092a2466447e9181a764c86f3c7732
marcelo-vitti <129317565+marcelo-vitti@users.noreply.github.com>
2023-03-29 17:06:26 -0300

    Create negotiated_coffee_average.sql

---
+++ sql/negotiated_coffee_average.sql
@@ -0,0 +1,19 @@
+SELECT
+  "yearly"                    AS analysis_interval,
+  strftime("%Y", coffee.Date) AS activity_date,
+  AVG(Volume)                 AS average_of_negotiated_coffee
+
+FROM coffee
+
+GROUP BY strftime("%Y", coffee.Date)
+
+UNION ALL
+
+SELECT
+  "monthly"                    AS analysis_interval,
+  strftime("%Y-%m", coffee.Date) AS activity_date,
+  AVG(Volume)                 AS average_of_negotiated_coffee
+
+FROM coffee
+
+GROUP BY strftime("%Y-%m", coffee.Date)
