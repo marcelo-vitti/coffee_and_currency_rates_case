e3f705a169923900a6a045c570c8ac6e20703716
marcelo-vitti <129317565+marcelo-vitti@users.noreply.github.com>
2023-03-30 13:29:33 -0300

    Improvement on query indentation

--- sql/negotiated_coffee_average.sql
+++ sql/negotiated_coffee_average.sql
@@ -10,9 +10,9 @@ GROUP BY strftime("%Y", coffee.Date)
 UNION ALL
 
 SELECT
-  "monthly"                    AS analysis_interval,
+  "monthly"                      AS analysis_interval,
   strftime("%Y-%m", coffee.Date) AS activity_date,
-  AVG(Volume)                 AS average_of_negotiated_coffee
+  AVG(Volume)                    AS average_of_negotiated_coffee
 
 FROM coffee
 
